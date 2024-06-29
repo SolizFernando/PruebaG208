23ecd7041d0f8ed317152108972b182de70e1e47
SolizFernando <166055762+SolizFernando@users.noreply.github.com>
2024-06-28 21:04:18 -0300

    Inicialización del proyecto vacío. Grupo 208

---
+++ src/main/java/ar/edu/unju/fi/PruebaG208Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaG208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaG208Application.class, args);
+	}
+
+}
